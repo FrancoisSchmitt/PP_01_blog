b021142b8e7ffa7be565462d45571c89d82b3596
Schmitt François <Schmitt.francois.1699@gmail.com>
2023-12-05 15:08:46 +0100

    adding UserLogin API

--- serveur/controller/authController.js
+++ serveur/controller/authController.js
@@ -1,10 +1,6 @@
-const Userdb = require("../model/userModel");
+const Userdb = require("../model/authModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
-/**
- *
- */
 
 exports.CreateUser = async (req, res) => {
   try {
@@ -27,4 +23,28 @@ exports.CreateUser = async (req, res) => {
   } catch (error) {
     res.status(400).send(error.message);
   }
+};
+
+exports.UserLogin = (req, res, next) => {
+    Userdb.findOne({ email: req.body.email })
+        .then((user) => {
+            if (!user) {
+                return res.status(401).json({ error: "Utilisateur non trouvé !" });
+            }
+            bcrypt
+                .compare(req.body.password, user.password)
+                .then((valid) => {
+                    if (!valid) {
+                        return res.status(401).json({ error: "Mot de passe incorrect !" });
+                    }
+                    res.status(200).json({
+                        userId: user.id,
+                        token: jwt.sign({ userId: user.id }, "RANDOM_TOKEN_SECRET", {
+                            expiresIn: "24h",
+                        }),
+                    });
+                })
+                .catch((error) => res.status(500).json({ error }));
+        })
+        .catch((error) => res.status(500).json({ error }));
 };
--- serveur/routes/router.js
+++ serveur/routes/router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const route = express.Router();
 const authController = require("../controller/authController");
 
-route.post("/auth/userLogin", authController.CreateUser);
+route.post("/auth/UserCreate", authController.CreateUser);
+route.post("/auth/UserLogin", authController.UserLogin);
 
 module.exports = route;
